d0c097998a5abae6dcb33fdf20efca20eab285c1
ccx <4190373@qq.com>
2026-08-10 10:39:57 +0800

    题目一

--- common/src/command_pack_queue.c
+++ common/src/command_pack_queue.c
@@ -13,34 +13,43 @@
  * 用 count 区分"空"和"满"。
  */
 
-/* TODO: 初始化队列，将 head/tail/count 归零 */
 void packet_queue_init(packet_queue *q)
 {
-    /* 在此实现 */
+    q->head = 0U;
+    q->tail = 0U;
+    q->count = 0U;
 }
 
-/* TODO: 入队，成功返回 true，队列满返回 false */
 bool packet_queue_push(packet_queue *q, const command_packet *pkt)
 {
-    /* 在此实现 */
+    if (packet_queue_is_full(q)) {
+        return false;
+    }
+    q->buf[q->tail] = *pkt;
+    q->tail = (q->tail + 1U) % PACKET_QUEUE_SIZE;
+    q->count++;
+    return true;
 }
 
-/* TODO: 出队，将数据写入 *pkt，成功返回 true，队列空返回 false */
 bool packet_queue_pop(packet_queue *q, command_packet *pkt)
 {
-    /* 在此实现 */
+    if (packet_queue_is_empty(q)) {
+        return false;
+    }
+    *pkt = q->buf[q->head];
+    q->head = (q->head + 1U) % PACKET_QUEUE_SIZE;
+    q->count--;
+    return true;
 }
 
-/* TODO: 判空 */
 bool packet_queue_is_empty(const packet_queue *q)
 {
-    /* 在此实现 */
+    return (q->count == 0U);
 }
 
-/* TODO: 判满 */
 bool packet_queue_is_full(const packet_queue *q)
 {
-    /* 在此实现 */
+    return (q->count == PACKET_QUEUE_SIZE);
 }
 
 /* ================================================================
